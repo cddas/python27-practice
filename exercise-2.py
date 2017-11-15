b83f9a5cb7ba482e57f173bc368dcb7503fe0bb1
dasc1 <Chandan.Das@in.pega.com>
2017-11-16 02:55:04 +0530

    Added exercise-2

---
+++ exercise-2.py
@@ -0,0 +1,9 @@
+input_number = raw_input("Enter any number : ")
+input_number = int(input_number)
+
+print("You entered " + str(input_number))
+if ((input_number % 2) == 0):
+    print("The entered number is an even number")
+else:
+    print("The entered number is an odd number")
+
